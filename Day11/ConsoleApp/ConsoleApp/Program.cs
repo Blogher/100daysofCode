ccfa89ccdd9c84572cbaebf52701cfabc204fe2f
[abidemimarufat@gmail.com] <abidemimarufat@gmail.com>
2020-12-13 16:12:25 +0100

    Day 11

---
+++ Day11/ConsoleApp/ConsoleApp/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace ConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random random = new Random();
+            //This prints 12 random letters from the alphabets
+            string[] randomLetters = new string[12];
+
+            for (int i = 0; i < randomLetters.Length; i++)
+            {
+                int lettersInUnicode = random.Next(65, 90);
+                string result = Convert.ToChar(lettersInUnicode).ToString();
+
+
+                randomLetters[i] = result;
+            }
+
+            foreach (var randomLetter in randomLetters)
+            {
+                Console.Write(randomLetter);
+            }
+        }
+        }
+}
---
+++ Day11/README.md
@@ -0,0 +1,7 @@
+# Day 11
+
+We making this project today. Yess!
+
+Ok, this was fun. Yes I googled a lot 😉😉 but this is fun. This project itself is a simple console app which prints random letters from the english alphabet. It seemed pretty easy in my head but I struggled with it nonetheless. I found out about the **Convert** class in Ms docs [here](https://docs.microsoft.com/en-us/dotnet/api/system.convert.tochar?view=net-5.0) which converts an int to a char (in my case).
+
+The link to the 
